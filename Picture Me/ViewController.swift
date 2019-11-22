e4b854b0daa951b8c333267fc946018f24bb7ae1
Narcis Zait <narcis.zait@gmail.com>
2019-11-22 19:32:10 +0100

    Initial Commit

---
+++ Picture Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Picture Me
+//
+//  Created by Narcis Zait on 22/11/2019.
+//  Copyright © 2019 Narcis Zait. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Picture MeTests/Picture_MeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Picture_MeTests.swift
+//  Picture MeTests
+//
+//  Created by Narcis Zait on 22/11/2019.
+//  Copyright © 2019 Narcis Zait. All rights reserved.
+//
+
+import XCTest
+@testable import Picture_Me
+
+class Picture_MeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
